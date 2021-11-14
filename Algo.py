c7246a769c20ba014e6b9bee49a5c44fba4e638e
eitan <eitan76543@gmail.co.il>
2021-11-14 13:01:10 +0200

    Algo

--- Algo.py
+++ Algo.py
@@ -3,30 +3,49 @@ import sys
 
 from Building import Building
 from CallOfElevator import CallOfElevator
+from Elevator import * #Elevator
 
 
 class Algo:
-    def __init__(self,string:str=None):
+    def __init__(self, string: str = None):
         list = string.split(" ")
-        b=Building()
-        self.building=b.from_json(list[0])
-        self.csv = list[1]
+        b = Building()
+        self.building = b.from_json(list[0])
+        self.csv = self.readcsv(list[1])
         self.out = list[2]
+        self.writeCsv(self.out, self.csv)
 
     def readcsv(file_name: str):
         with open(file_name) as file:
-            rows = []
-            a = []
+            CallsList = []
             csvreader = csv.reader(file)
             for row in csvreader:
-                c = CallOfElevator(name=row[0], Time=row[1], src=row[2],
-                                   dest=row[3], sta=row[4], ele=int(row[5]))
-                Algo.allocate(c)
+                c = CallOfElevator.CallOfElevator(name=row[0], Time=float(row[1]), src=int(row[2]),
+                                   dest=int(row[3]), sta=row[4],
+                                   ele=int(row[5]))
+                c.ele = Algo.allocate(c)
+                CallsList.append(c)
+            return CallsList
 
+    def allocate(call: CallOfElevator=None):
+        # b = Building()
+        # b.from_json("B2.json")
+        # i=0
+        # for j in b.el:
+        #     return min(b.el[i],b.el[i+1],)
+        # i=0
+        # for elev in self.building.el:
+        #     if elev.calls[i].dest<call.dest:
+        #         return 0
+        #     elif elev.calls[i].dest==call.dest:
+        #         return 1
+        #     else:
+        #         return 2
+        if call.src != -4 and call.Time<90.155555555:
+            return 0
+        else:
+            return 1
 
-
-    def allocate(self, call:CallOfElevator=None):
-        return
     #     min=sys.maxint
     #     j=0
     #     count=0
@@ -39,29 +58,28 @@ class Algo:
     #     writeCsv(call,j)
     #     addCall(j,call)
 
-def writeCsv(file,list):
-    new_list=[]
-    for obj in list:
-        new_list.append(obj.__dict__.values())
-    with open(file,'w',newline="") as f:
-        csvwriter=csv.writer(f)
-        csvwriter.writerows(new_list)
-        print(new_list)
+    def writeCsv(file, list):
+        new_list = []
+        for obj in list:
+            new_list.append(obj.__dict__.values())
+        with open(file, 'w', newline="") as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerows(new_list)
+            print(new_list)
+
 
 if __name__ == '__main__':
-    Algo.readcsv("Calls_d.csv")
-#     a=Algo("<Building.json> <Calls.csv> <output.csv>")
-    with open("Calls_a.csv") as file:
-        a = []
-        csvreader = csv.reader(file)
-        for row in csvreader:
-            c = CallOfElevator(name=row[0], Time=row[1], src=row[2], dest=row[3], sta=row[4],
-                           ele=int(row[5]))
-            a.append(c)
-        for i in a:
-            print(i.ele)
-            print(i.dest)
+    a = Algo.readcsv("Calls_b.csv")
+    Algo.writeCsv("try.csv", a)
 
-        #readcsv1("Calls_b.csv")
-
-        writeCsv("try.csv",a)
+    # a = Algo("<Building.json> <Calls.csv> <output.csv>")
+    #     with open("Calls_a.csv") as file:
+    #         a = []
+    #         csvreader = csv.reader(file)
+    #         for row in csvreader:
+    #             c = CallOfElevator(name=row[0], Time=row[1], src=row[2], dest=row[3], sta=row[4],
+    #                            ele=0)
+    #             a.append(c)
+    #         for i in a:
+    #             print(i.ele)
+    #             print(i.dest)
