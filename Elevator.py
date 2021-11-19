61485ea1454cff2766fa53a3926ae58d519b13cd
noamya-sh <noamyashany.gmail.com>
2021-11-19 13:01:32 +0200

    test

--- Elevator.py
+++ Elevator.py
@@ -1,7 +1,6 @@
 import numpy as np
 import math
 from CallOfElevator import *
-import copy
 class Elevator:
 
     def __init__(self, _id:int=None, _speed:float=None, _minFloor:int=None,
@@ -18,11 +17,6 @@ class Elevator:
         self.fs= {}
         self.call2test=None
 
-    # def __str__(self) -> str:
-    #     return f"id:{self._id}, speed:{self._speed}, minFloor:{self._minFloor}, " \
-    #            f"maxFloor:{self._maxFloor}, closeTime:{self._closeTime}, " \
-    #            f"openTime:{self._openTime}, startTime:{self._startTime}, stopTime:{self._stopTime}"
-
     def __add__(self, call:CallOfElevator):
         self.addfloorsStop(self.fs,call)
         return self
@@ -33,20 +27,43 @@ class Elevator:
     def __lt__(self, other):
         return self.time(self.call2test) < other.time(self.call2test)
 
-    def addfloorsStop(self,dict,call:CallOfElevator):
+    def addfloorsStop(self,dict:{},call:CallOfElevator)->None:
+        """
+        :param dict: dict put new stops in it.
+        :param call: call to insert in dict.
+        """
         time2src, time2dest = self.reachFloor(dict,call.src, call.dest, call.Time)
         self.insetKV(dict, call.src,time2src,call.dest,time2dest)
 
     def disInFloor(self,travelTime:float)->float: # speed * time = distance
+        """
+        :param travelTime: time.
+        :return distance that the elevator performs at a certain time.
+        """
         return self._speed * travelTime
 
-    def disInTime(self,distance):
+    def disInTime(self,distance:int)->float:
+        """
+        :param distance: distance of floors.
+        :return time required to travel a certain distance.
+        """
         return distance/self._speed
 
-    def stop1(self):
+    def stop1(self)->float:
+        """
+        :return time of stop in floor.
+        """
         return self._stopTime+self._openTime+self._closeTime+self._startTime
 
-    def getind(self,list,floor):
+    def getind(self,list:list[int],floor:int)->int:
+        """
+        This function returns the index in which the input floor will be inserted.
+        If is possible to insert the floor in a way that does not extend
+        the way to other floors - insert there, otherwise insert at the end.
+        :param list: list of floors.
+        :param floor: floor to insert.
+        :return index.
+        """
         l = np.array(list)
         diff = abs(l[1:] - l[:-1])
         ls = abs(l[1:] - floor) + abs(floor - l[:-1])
@@ -55,27 +72,16 @@ class Elevator:
                 return i+1;
         return len(l)
 
-    # def pos(self,dict,time):
-    #     dic1 = {k: v for k, v in dict.items() if k > time}
-    #     dic2 = {k: v for k, v in dict.items() if k <= time}
-    #     if len(dic1) == 0 and len(dic2) == 0:
-    #         return 0
-    #     if len(dic2) > 0:
-    #         if len(dic1)>0:
-    #             x = dic1[min(dic1)]-dic2[max(dic2)]
-    #             if x > 0:
-    #                 return dic2[max(dic2)] + self.disInFloor(math.ceil(time - max(dic2) - self._startTime))+self.stopInFloors()
-    #             else:
-    #                 return dic2[max(dic2)] - self.disInFloor(math.ceil(time - max(dic2) - self._startTime))-self.stopInFloors()
-    #         else:
-    #             return dic2[max(dic2)]
-    #     else:
-    #         if dic1[min(dic1)]>0:
-    #             return self.disInFloor(math.ceil(time - self._startTime))+self.stopInFloors()
-    #         else:
-    #             return 0-self.disInFloor(math.ceil(time - self._startTime))-self.stopInFloors()
 
-    def reachFloor(self,dict,src : int, dest : int, timeInit : float):#return the time that elevator reach the floors
+    def reachFloor(self,dict,src : int, dest : int, timeInit : float)->tuple:#return the time that elevator reach the floors
+        """
+        This function will look for the time when the elevator will reach the src floor and the dest floor.
+        :param dict: dict that from its data the times will be calculated.
+        :param src: src floor of the callForElevator.
+        :param dest: dest floor of the callForElevator.
+        :param timeInit: time init of the callForElevator.
+        :return :t1: time reach src floor. t2:time reach dest floor.
+        """
         t1 = t2 = timeInit
         if not dict:
             t1 += self.disInTime(abs(src))
@@ -85,7 +91,7 @@ class Elevator:
             return math.ceil(t1), math.ceil(t2)
         dic1 = {k: v for k, v in dict.items() if k > timeInit}
         dic2 = {k: v for k, v in dict.items() if k < timeInit}
-        if not dic1:
+        if not dic1: #the elevator in LEVEL state
             pos = dic2[max(dic2)]
             t1 = max(dic2) + self.disInTime(abs(src-pos))+self.stop1()
             t2 = math.ceil(t1) + self.disInTime(abs(dest-src))+self.stop1()
@@ -94,6 +100,7 @@ class Elevator:
             dic1_keys = list(sorted(dic1.keys()))
             dic1_values = list(dic1.values())
             t1 = dic1_keys[ind-1] + self.disInTime(abs(dic1_values[ind-1]-src))+self.stop1()
+            # find time for dest floor:
             if len(list(dic1.values())) > ind:
                 ind2 = self.getind(list(dic1.values())[ind:], dest)
                 dic1_keys = list(sorted(dic1.keys()))[ind:]
@@ -103,14 +110,22 @@ class Elevator:
                 t2 = math.ceil(t1) + self.disInTime(abs(dest-src))+self.stop1()
         return math.ceil(t1),math.ceil(t2)
 
-    def insetKV(self,dict,f1,t1,f2,t2):
+    def insetKV(self,dict:{},f1:int,t1:int,f2:int,t2:int)->None:
+        """
+        This function insert new stops to dict and updates the times of subsequent stops.
+        :param dict: dict put new stops in it.
+        :param f1: src floor.
+        :param t1: time reach src floor.
+        :param f2: dest floor.
+        :param t2: time reach dest floor.
+        """
         dic = {k: v for k, v in dict.items() if t1 <= k <= t2}
         if t1 and t2 in dic:
             return
         if t1 not in dic:
             for k,v in dic.items():
                 if k<t2:
-                    dict[math.ceil(k+self.stop1())] = dict.pop(k)
+                    dict[k+math.ceil(self.stop1())] = dict.pop(k)
             dict[t1] = f1
         if t2 not in dic:
             dic2 = {k: v for k, v in dict.items() if t2 < k}
@@ -118,10 +133,11 @@ class Elevator:
                 dict[k+2*math.ceil(self.stop1())] = dict.pop(k)
             dict[t2] = f2
 
-    def stopInFloors(self):
-        return self.disInFloor(self._stopTime)
-
-    def cleanfs(self,time):
+    def cleanfs(self,time)->None:
+        """
+        This function deletes all stops that are reached before 'time', except for the last stop
+        :param time: the decisive time.
+        """
         dic = {k: v for k, v in self.fs.items() if k < time}
         if len(dic)>0:
             x = max(dic)
@@ -129,45 +145,41 @@ class Elevator:
                 if k!=x:
                     self.fs.pop(k)
 
-    def time(self,call:CallOfElevator):
-        if not self.fs:
+    def time(self,call:CallOfElevator)->int:
+        """
+        This function calculate the cost of insert new call to this elevator.
+        :param call: callForElevator to check.
+        """
+        if not self.fs: #in case of start simulator
             self.addfloorsStop(self.fs,call)
             x = max(self.fs) - call.Time
             self.fs= {}
             return x
-
         callInit = call.Time
         self.cleanfs(callInit)
-        i=0
         dic = {k: v for k, v in self.fs.items() if k > callInit}
-        d2 = copy.deepcopy(dic)
-        t1, t2 = self.reachFloor(d2, call.src, call.dest, call.Time)
-        timeTask = t2 - call.Time
-
-        self.addfloorsStop(d2, call)
-        d3 = {k: v for k, v in dic.items() if not (k == t1 and v== call.src) and not (k ==t2 and v==call.dest)}
-        l=[]
-        l.append(math.ceil(callInit))
-        list1 = l + list(sorted(dic.keys()))
-        # print(list1)
-        list2 = l + list(sorted(d3.keys()))
-        # print(list2)
-        s1=self.calDiff(list1)
-        s2=self.calDiff(list2)
-        # if len(list2) != len(list1):
-        #     print(callInit)
-        #     print(list1)
-        #     # print(np.cumsum(np.diff(list1)))
-        #     print(list2)
-            # print(np.cumsum(np.diff(list2)))
-        #     print("*************************")
-        # s = 0
-        # for i in range(len(list2)):
-        #     s+=list2[i]-list1[i]
-        # cost = np.cumsum(np.diff(list2)).sum() - np.diff(list1).sum()
-
+        c = list(dic.values())
+        f1 = self.getind(c,call.src)
+        f2 = self.getind(c, call.dest)
+        i=f1
+        b=[math.ceil(callInit)]
+        for i in range(len(c)):
+            if i <=f2:
+                b.append(c[i]+math.ceil(self._stopTime))
+            else:
+                b.append(c[i] + 2*math.ceil(self._stopTime))
+        t1, t2 = self.reachFloor(dic, call.src, call.dest, call.Time)
+        timeTask = math.ceil(t2 - call.Time)
+        c = [math.ceil(callInit)]+c
+        s1=self.calDiff(c)
+        s2=self.calDiff(b)
         cost = s2-s1
         return cost + timeTask
     #
-    def calDiff(self,list):
+    def calDiff(self,list:list[int])->int:
+        """
+        This function calculate differnce of each element [1-end] with first element.
+        :param list: list of int.
+        :return: list of differences.
+        """
         return sum([list[i] - list[0] for i in range(1, len(list))])
